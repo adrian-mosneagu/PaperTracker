0b0a5021c9e1e469ac117edbf98452c31036603b
Adrian Mosneagu <adrian.mosneagu@3pillarglobal.com>
2019-11-03 18:17:41 +0200

    Added flow for all actions

--- app/src/main/java/com/papertracker/activities/EditItemActivity.java
+++ app/src/main/java/com/papertracker/activities/EditItemActivity.java
@@ -1,9 +1,14 @@
 package com.papertracker.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -24,6 +29,35 @@ public class EditItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_activity);
 
+        ImageButton cancel = findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(EditItemActivity.this).setTitle("Cancel?")
+                        .setMessage("Item will not be saved!")
+                        .setIcon(android.R.drawable.stat_sys_warning)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // TODO: Add toast on main activity for cancel
+                                finish();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                dialog.cancel();
+                            }
+                        }).show();
+            }
+        });
+
+        Button save = findViewById(R.id.saveDoc);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Add item to DB, add toast on main activity for cancel
+                finish();
+            }
+        });
 
         docList = new ArrayList<>();
         docList.add(new Document("test", new Date()));
@@ -32,9 +66,11 @@ public class EditItemActivity extends AppCompatActivity {
         rvDocs.setAdapter(adapter);
         rvDocs.setLayoutManager(new LinearLayoutManager(EditItemActivity.this));
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         FloatingActionButton fab = findViewById(R.id.addDocumentButton);
 
-        // TODO: This behaviour should be done when clicking on save/cancel buttons
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
